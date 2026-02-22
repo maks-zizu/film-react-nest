704f413ac1128c17f5911d57cdcfce9b55e5efd9
Maksim Zhovna <m.zhovna@1440.space>
2026-02-22 16:14:35 +0300

    fix: правки порта

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
-  await app.listen(3000);
+
+  const port = Number(process.env.PORT ?? 3000);
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
