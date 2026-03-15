30866df2ff53eaaf2c6f44724b92ad6f91b83364
Maksim Zhovna <m.zhovna@1440.space>
2026-03-15 23:48:17 +0300

    fix: исправить base path фронтенда для сервера и pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/film-react-nest/",
+  base: process.env.VITE_BASE_PATH ?? "/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
